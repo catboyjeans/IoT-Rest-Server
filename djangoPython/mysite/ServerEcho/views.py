c3436aecac3dee0589efe0c4a718e27229d694e6
Embedded_Cat <neko@neko.localdomain>
2023-02-13 19:37:12 -0600

    Minor changes

--- djangoPython/mysite/ServerEcho/views.py
+++ djangoPython/mysite/ServerEcho/views.py
@@ -11,7 +11,7 @@ def URL_handler(request):
     temp= request.GET
     #could add handle by checking the dict value per value
     temp_vec=[  
-    temp.get('s1',0),
+    temp.get('s1',0),   
     temp.get('s2',0),
     temp.get('s3',0),
     temp.get('s4',0),
@@ -21,9 +21,9 @@ def URL_handler(request):
     ]
 
     vector_state={'element{}'.format(j):temp_vec[j] for j in range(len(temp_vec))}
-    argDict={'Vector State: ' : vector_state}
+    # argDict={'Vector State: ' : vector_state}
 
-    return argDict
+    return vector_state
 
 #tutorial page 
 def index(request):
@@ -36,8 +36,18 @@ def recieve_message(request, thing_name):
 
     if Person.objects.filter(first_name=thing_name).exists():
         query.update({'Debugg State':'Object found in database, value assigned to object'})
+        p1=Person.objects.filter(first_name=thing_name).first()
+       # Change database value
+        p1.state1 = query['element0']
+        p1.state2 = query['element1']
+        p1.state3 = query['element2']
+        p1.state4 = query['element3']
+        p1.state5 = query['element4']
+        p1.state6 = query['element5']
+        p1.state7 = query['element6']
 
-        # Change database value
+        p1.save()
+        
 
     else:
         query.update({'Debugg State':'Object not found in database, creating object ... '})
@@ -82,3 +92,6 @@ def get_data(request, thing_name):
     query.update({'All Data' : 'This is a Placeholder'})
         
     return JsonResponse(query)
+
+    def default_view(request):
+        return HttpResponse('Server is up and runnin!! ^_^')
