fb70bc4aad2d65dd17553bd32b2e284e52987964
Neko-WinGit <davidg0704@hotmail.com>
2023-03-07 00:10:51 -0600

    fixed INSTALLED_APPS

--- djangoPython/mysite/mysite/settings.py
+++ djangoPython/mysite/mysite/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'ServerEcho.apps.ServerechoConfig',
+    # 'ServerEcho.apps.ServerechoConfig',
+    'ServerEcho',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
